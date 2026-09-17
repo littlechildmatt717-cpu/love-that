6c1a9a4dd1070fca68893852a6be00680df86ec7
littlechildmatt717-cpu <littlechildmatt717@gmail.com>
2026-09-17 14:21:25 +0100

    Refactor capacitor.config.ts to use TypeScript

--- packages/capacitor.config.ts
+++ packages/capacitor.config.ts
@@ -1,15 +1,19 @@
-{
-  "appId": "com.lovethat.app",
-  "appName": "love that",
-  "webDir": "dist",
-  "server": {
-    "androidScheme": "https"
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lovethat.app',
+  appName: 'love that',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
   },
-  "android": {
-    "manifest": {
-      "$": {
-        "xmlns:android": "http://schemas.android.com/apk/res/android"
+  android: {
+    manifest: {
+      '$': {
+        'xmlns:android': 'http://schemas.android.com/apk/res/android'
       }
     }
   }
-}
+};
+
+export default config;
