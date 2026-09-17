539e97dcafb5f0db7c23acd34e32d3bea5552595
littlechildmatt717-cpu <littlechildmatt717@gmail.com>
2026-09-17 22:10:18 +0100

    Delete packages/capacitor.config.ts

--- packages/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.meetdating.app',
-  appName: 'love that',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  server: { androidScheme: 'https' }
-};
-export default config;
