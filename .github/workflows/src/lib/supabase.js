48f8fb89a432de85eb13e19002a4b60984d42b6e
littlechildmatt717-cpu <littlechildmatt717@gmail.com>
2026-09-23 22:25:46 +0100

    Delete .github/workflows/src/lib/supabase.js

--- .github/workflows/src/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const SUPABASE_URL = 'https://igsucrszgotwrifbrvxd.supabase.co'
-export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_8wR49vTI4RO90t9ctzq8uA_ApfidSbJ'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true },
-})
